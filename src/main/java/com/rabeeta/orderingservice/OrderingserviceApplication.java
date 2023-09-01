20b20053aad55f30ca8a0e40e4e83f7d1177bf3e
serialcoder <rforrizwan55@gmail.com>
2023-09-01 13:28:32 -0500

    first commit

---
+++ src/main/java/com/rabeeta/orderingservice/OrderingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rabeeta.orderingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingserviceApplication.class, args);
+	}
+
+}
