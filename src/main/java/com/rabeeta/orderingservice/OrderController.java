d83e2ad2fc4931e55efe52dce063d63e1bea2ef0
raqeeb <rforrizwan55@gmail.com>
2023-09-04 14:11:27 -0500

    hello

--- src/main/java/com/rabeeta/orderingservice/OrderController.java
+++ src/main/java/com/rabeeta/orderingservice/OrderController.java
@@ -1,5 +1,14 @@
 package com.rabeeta.orderingservice;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
+
+    @GetMapping("/")
+    public String home(){
+        return "hello";
+    }
 }
